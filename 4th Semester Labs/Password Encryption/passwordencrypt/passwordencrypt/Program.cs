a6f37c33d06b42e6a040f8f5866636f9f153ee88
Muhammad Zain <mzainjed@gmail.com>
2024-06-22 15:48:16 +0500

    Add files via upload

---
+++ 4th Semester Labs/Password Encryption/passwordencrypt/passwordencrypt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace passwordencrypt
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+            pass p = new pass();
+            p.EncryptPass(ref password);
+            Console.WriteLine(password);
+        }
+        
+    }
+    class pass
+    {
+        public void EncryptPass(ref string password)
+        {
+            var sha = SHA256.Create();
+            var ByteArr = Encoding.Default.GetBytes(password);
+            var encryptPass = sha.ComputeHash(ByteArr);
+
+            password = Convert.ToBase64String(encryptPass);
+        }
+    }
+}
